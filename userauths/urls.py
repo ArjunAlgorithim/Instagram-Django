9e1fdc69cced8ccfa170bc56d3e4c5c5cacc11de
Arun Menon <menonarun23@gmail.com>
2024-02-28 19:06:17 +0530

    new changes

--- userauths/urls.py
+++ userauths/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('sign-up/', views.register, name="sign-up"),
     path('sign-in/', auth_views.LoginView.as_view(template_name='sign-in.html', redirect_authenticated_user=True), name='sign-in'),
     path('sign-out/',views.CustomLogoutView.as_view(),name='sign-out'), 
+    path('reset_password/',auth_views.PasswordResetView.as_view(),name="reset_password"), 
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"), 
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"), 
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"), 
     
 
 
